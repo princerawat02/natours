f45de3b00be74ee83a14714e4d94885ddc6cba44
Prince <princerawat70095@gmail.com>
2024-12-22 19:38:24 +0530

    section 8 complete
    using mongoDB with mongoose.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 //MIDDLEWARE (manipulating req.query)
 exports.aliasTopTours = (req, res, next) => {
@@ -10,50 +11,13 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFeilds = ['page', 'sort', 'limit', 'fields'];
-    excludedFeilds.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advance filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2)  SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1; //3
-    const limit = req.query.limit * 1 || 100; //3
-    const skip = (page - 1) * limit; //(3 - 1) * 3 => 2 * 3 => 6;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
