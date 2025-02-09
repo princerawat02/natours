44496a5589afa876ab79970e6508442b4d765ac7
Prince <princerawat707@gmail.com>
2025-02-09 16:42:02 +0530

    setting up project structure

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
 
@@ -70,20 +71,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base");
-});
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All tours",
-  });
-});
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The forest hicker tour",
-  });
-});
-
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
