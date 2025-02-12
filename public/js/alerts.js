adc2c02c5a2803dcc4f65e569a7555e08d924c44
Prince <princerawat707@gmail.com>
2025-02-12 09:59:15 +0530

    logging in user Part-2 complete

---
+++ public/js/alerts.js
@@ -0,0 +1,12 @@
+export const hideAlert = () => {
+  const el = document.querySelector(".alert");
+  if (el) el.parentElement.removeChild(el);
+};
+
+// type is 'success' or 'error'
+export const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${getTestMessageUrl}</div>`;
+  document.querySelector("body").insertAdjacentElement("afterbegin", markup);
+  window.setTimeout(hideAlert, 5000);
+};
---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "/api/v1/users/login",
+      data: {
+        email,
+        password,
+      },
+    });
+    //Show the alert and reload the page
+    if (res.data.status === "success") {
+      showAlert("success", "Logged in successfully!");
+      window.setTimeout(() => {
+        location.assign("/");
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert("error", err.response.data.message);
+  }
+};
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  login(email, password);
+});
