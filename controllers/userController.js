b7b427cc4d9a575a23f94e876166568375c22b96
Prince <princerawat7700@gmail.com>
2025-02-14 17:55:58 +0530

    image uploads using Multer & configure it

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,38 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    // user-23423asd23-223333.jpeg
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), true);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
-  Object.keys(obj).forEach(el => {
+  Object.keys(obj).forEach((el) => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
   return newObj;
@@ -17,6 +44,8 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  // console.log(req.body);
+  // console.log(req.file);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -33,14 +62,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
-    runValidators: true
+    runValidators: true,
   });
 
   res.status(200).json({
     status: 'success',
     data: {
-      user: updatedUser
-    }
+      user: updatedUser,
+    },
   });
 });
 
@@ -49,14 +78,14 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 
   res.status(204).json({
     status: 'success',
-    data: null
+    data: null,
   });
 });
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not defined! Please use /signup instead'
+    message: 'This route is not defined! Please use /signup instead',
   });
 };
 
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const viewsController = require("../controllers/viewController");
-const authController = require("../controllers/authController");
+const express = require('express');
+const viewsController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 // router.use(authController.isLoggedIn);
 
-router.get("/", authController.isLoggedIn, viewsController.getOverview);
-router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
-router.get("/login", authController.isLoggedIn, viewsController.getLoginForm);
-router.get("/me", authController.protect, viewsController.getAccount);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 // router.patch(
 //   "/submit-user-data",
